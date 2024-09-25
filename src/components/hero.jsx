5ad9106941ab29dd795649ec0426f07675c928c5
Reon1917 <linmyatphyo03@gmail.com>
2024-09-25 18:34:18 +0630

    scaffold done

---
+++ src/components/hero.jsx
@@ -0,0 +1,26 @@
+// components/Hero.jsx
+import React from 'react';
+
+function Hero() {
+    return (
+        <div className='border-blue-600'>
+            <h1>Hero Section</h1>
+            {<div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Mr Pyae Wai Phyo</h1>
+                        <p className="py-6">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                    </div>
+                </div>
+            </div>}
+        </div>
+    );
+}
+
+export default Hero;
